86219a0942a01d30296aaa76a0f13fafa43c9e47
EmilyC <eclairebenn@outlook.com>
2021-12-05 10:24:00 -0800

    Use TypeScript in Mock DB creation

--- tools/createMockDb.ts
+++ tools/createMockDb.ts
@@ -1,11 +1,11 @@
 /* eslint-disable no-console */
 const fs = require("fs");
-const path = require("path");
+const Path = require("path");
 const mockData = require("./mockData");
 
-const { tickers } = mockData;
-const data = JSON.stringify({ tickers });
-const filepath = path.join(__dirname, "db.json");
+const { Tickers } = mockData;
+const data = JSON.stringify({ Tickers });
+const filepath = Path.join(__dirname, "db.json");
 
 fs.writeFile(filepath, data, function (err) {
   err ? console.log(err) : console.log("Mock DB created.");
